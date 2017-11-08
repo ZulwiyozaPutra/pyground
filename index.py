cb7d9c91ba3f28fa597d7dfe857cbeef812e3e7d
Zulwiyoza Putra <zulwiyozaputra@Taylor.local>
2017-11-08 20:16:08 +0700

    added collection

--- index.py
+++ index.py
@@ -29,16 +29,37 @@ def multiply(num1, num2):
 multiplication_result = multiply(2, 3)
 print(multiplication_result)
 
+# collection
 
-# declaring array
-grades = [77, 80, 90, 95, 100, 105, 107]
+# declaring an array/list ordered & mutable
+array_grades = [77, 80, 90, 95, 100, 105, 107]  # ordered and mutable
+array_grades.append(200)
 
 
-# declaring a method to find mean of an array
-def mean(array):
+# declaring a tuple
+tuple_grades = (77, 80, 90, 95, 100, 105, 107)  # ordered and immutable
+tuple_grades = tuple_grades + (200,)
+
+
+# declaring a set
+set_grades = {77, 80, 90, 95, 100, 105, 107}  # unordered and mutable
+set_grades.add(200)
+
+
+# declaring a method to find mean of an array & tuple
+def mean_from_array(array):
     mean = sum(array) / len(array)
     return mean
 
 
-grades_mean = mean(grades)
-print(grades_mean)
+def mean_from_tuple(tuple):
+    mean = sum(tuple) / len(tuple)
+    return mean
+
+
+# both are works almost exactly alike
+grades_mean_from_array = mean_from_array(array_grades)
+print(grades_mean_from_array)
+
+grades_mean_from_tuple = mean_from_tuple(tuple_grades)
+print(grades_mean_from_tuple)
